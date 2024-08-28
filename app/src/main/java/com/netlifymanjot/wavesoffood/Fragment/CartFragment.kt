95fb26b392c222a435c0f9614a4753f8a6e317db
Jot <manjotsinghpnp@gmail.com>
2024-08-28 05:06:19 -0600

    Completed Day 4: Added and refined cart section with item management features.

--- app/src/main/java/com/netlifymanjot/wavesoffood/Fragment/CartFragment.kt
+++ app/src/main/java/com/netlifymanjot/wavesoffood/Fragment/CartFragment.kt
@@ -5,20 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.netlifymanjot.wavesoffood.R
+import com.netlifymanjot.wavesoffood.adapter.CartAdapter
+import com.netlifymanjot.wavesoffood.databinding.FragmentCartBinding
 
 class CartFragment : Fragment() {
 
-
+private lateinit var binding:FragmentCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+        val cartFoodName = listOf("Burger", "sandwich", "momo", "item", "noodles")
+        val cartItemPrice = listOf("$5", "$6", "$7", "$8","$10" )
+        val cartImage = listOf(
+            R.drawable.menu1,
+            R.drawable.menu2,
+            R.drawable.menu3,
+            R.drawable.menu4,
+            R.drawable.menu5,
+
+        )
+        val adapter = CartAdapter(ArrayList(cartFoodName), ArrayList(cartItemPrice), ArrayList(cartImage))
+        binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.cartRecyclerView.adapter = adapter
+        return binding.root
     }
 
     companion object {
